e2e88fd41cff4fa7914406799d93ab684d01e0e4
guenhy <eia.andersson@gmail.com>
2015-09-28 23:38:05 +0200

    Added comments to one more file

--- template/cart_template.php
+++ template/cart_template.php
@@ -1,22 +1,26 @@
 <?php 
 
 function printCart($items){
-
+	// If the cart is empty print string
 	if(empty($items)){
 		print "Varukorgen är tom.";
 		return;
 	}
 
+	// Loop items and add text
 	foreach ($items as $key => $value) {
 		$item = $key . " " . $value . " st";
 		$all[] = $item;
 	}
+
 	$arraytostring = implode(', ', $all);
 
+	// calculateCart located in inc/store.php
 	$cost = calculateCart($items);
 
 	$list = $arraytostring . ' - ';
 
+	// Concatenate the strings
 	$all = $list . $cost . ' kr <a href="emptycart.php"><button class="empty-cart-button button-xsmall pure-button">Töm</button></a>';
 
 	print $all;
